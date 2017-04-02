228b52477d7fc37214dc01dd80ca7f009a597c27
freemann350 <alex.gaspar@live.com.pt>
2017-04-02 23:35:58 +0100

    login-add

---
+++ Shared/conn.php
@@ -0,0 +1,23 @@
+﻿<?php //db conn
+
+	$sv = "localhost";
+
+	$user = "root";
+
+	$pw = "";
+
+	$db = "intervencoes_db";
+
+
+
+	$con = new mysqli($sv, $user, $pw, $db);
+
+
+
+	if ($con->connect_error) {
+
+		die("Erro de Ligação:" . $con->connect_error);
+
+	}
+
+	return true;
--- index.php
+++ index.php
@@ -1,36 +1,62 @@
 <?php
-$titulo = "Login";
-$backstretchInclude = True;
-?>
+	$titulo = "Login";
+	$backstretchInclude = True;
+	include 'Shared/conn.php';
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form   
+      $usr = mysqli_real_escape_string($db,$_POST['username']);
+      $pwd = mysqli_real_escape_string($db,$_POST['password']); 
+      
+      $sql = "SELECT * FROM professores WHERE username = '$usr' and passcode = '$pwd'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $usr and $pwd, table row must be 1 row
+		
+      if($count == 1) {
+         session_register("usr");
+         $_SESSION['login_user'] = $usr;
+         
+         header("location: Inicial");
+      }else {
+         $error = "Utilizador/Palavra-Passe errada";
+      };
+   };
+?>	
 <!DOCTYPE html>
 <html lang="pt">
 
 <?php #HEADER INCLUDE
-      include 'Shared/Head.php'
+      include 'Shared/Head.php';
 ?>
 
 <body>
     <section id="container">
             <div class="container">
-                <form class="form-login" action="Inicial">
+                <form class="form-login" action="" method = "post">
                     <h2 class="form-login-heading">Intervenções AESM</h2>
                     <div class="login-wrap">
-                        <input type="text" class="form-control" placeholder="Utilizador" autofocus>
+                        <input type="text" class="form-control" placeholder="Utilizador" name="username" autofocus>
                         <br>
                         <div class="form-group has-error has-feedback">
-                            <input type="password" class="form-control" id="inputError" placeholder="Palavra-passe">
+                            <input type="password" class="form-control" id="inputError" placeholder="Palavra-passe" name="password">
                             <br>
                             <button class="btn btn-theme btn-block" href="Inicial" type="submit"><i class="fa fa-lock"></i> Entrar</button>
                         </div>
                         <hr>
-                        <p style="text-align: center; color: #FF0000">Utilizador/Palavra-Passe inválida</p>
+                        <p style="text-align: center; color: #FF0000">
+                        <?php if (isset($error)) { echo $error; };?></p>
                     </div>
                 </form>
             </div>
     </section>
 
     <?php #HEADER INCLUDE
-          include 'Shared/Scripts.php'
+          include 'Shared/Scripts.php';
     ?>
 </body>
 
