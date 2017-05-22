90393009c2ba0c76fc937af7991791ce67f4f06b
freemann350 <alex.gaspar@live.com.pt>
2017-05-22 18:01:54 +0100

    Add files via upload

--- Shared/Header.php
+++ Shared/Header.php
@@ -1,6 +1,6 @@
 <header class="header black-bg">
     <div class="sidebar-toggle-box unselectable">
-        <div class="fa fa-bars tooltips" data-toggle="tooltip" title="Teste"></div>
+        <div class="fa fa-bars" title="Acionar barra lateral"></div>
     </div>
-    <a href="Inicial" class="logo unselectable tooltips" style="cursor: pointer;"><b>INTERVENÇÕES AESM</b></a>
+    <a href="Inicial" title="Ir para a página inicial" class="logo unselectable" style="cursor: pointer;"><b>INTERVENÇÕES AESM</b></a>
 </header>
