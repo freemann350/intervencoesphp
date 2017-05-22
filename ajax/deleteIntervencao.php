e878c99dddc6d0dd1e8a656a291cbd1abc4ebfa2
freemann350 <alex.gaspar@live.com.pt>
2017-05-22 17:48:26 +0100

    Add files via upload

---
+++ ajax/deleteIntervencao.php
@@ -0,0 +1,17 @@
+<?php
+  require_once("../Shared/conn.php");
+
+  if (isset($_GET["id"])) {
+    $Id = $_GET["id"];
+
+    $stmt = $con->prepare("UPDATE pedidos INNER JOIN intervencoes ON pedidos.Id = intervencoes.IdPedido SET pedidos.Resolvido = 0 WHERE intervencoes.Id = ?");
+    $stmt->bind_param("i", $Id);
+    $stmt->execute();
+
+    $stmt = $con->prepare("DELETE FROM intervencoes WHERE Id = ?");
+    $stmt->bind_param("i", $Id);
+    $stmt->execute();
+    echo "1";
+  } else {
+    echo "0";
+  }
---
+++ ajax/inativarUtilizador.php
@@ -0,0 +1,16 @@
+<?php
+  require_once("../Shared/conn.php");
+
+  if (isset($_GET["id"])) {
+    $Id = $_GET["id"];
+
+    $stmt = $con->prepare("UPDATE professores SET Ativo = '0' WHERE Id = ?");
+    $stmt->bind_param("i", $Id);
+    $stmt->execute();
+
+  if (mysqli_affected_rows($con) > 0) {
+    echo "1";
+  } else {
+    echo "0";
+  }
+};
