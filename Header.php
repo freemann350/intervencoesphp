04e263fb1a4618019bb943a7ba3d2168c58d9e02
freemann350 <alex.gaspar@live.com.pt>
2017-05-22 17:59:52 +0100

    Delete Header.php

--- Header.php
+++
@@ -1,6 +0,0 @@
-<header class="header black-bg">
-    <div class="sidebar-toggle-box unselectable">
-        <div class="fa fa-bars" title="Acionar barra lateral"></div>
-    </div>
-    <a href="Inicial" title="Ir para a página inicial" class="logo unselectable" style="cursor: pointer;"><b>INTERVENÇÕES AESM</b></a>
-</header>
