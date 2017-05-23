523bd18bd61dcac9e3ac13dcfd1b5b7edf526ed1
freemann350 <alex.gaspar@live.com.pt>
2017-05-23 13:56:43 +0100

    Delete filtrosanimate.js

--- assets/template/js/filtrosanimate.js
+++
@@ -1,9 +0,0 @@
-$('#filtrosheader').click(function() {
-    $('#filtrosdiv').slideToggle(450, function() {
-        if ($(this).css("display") == "none") {
-            $("#caret-spin").css("transform", "initial");
-        } else {
-            $("#caret-spin").css("transform", "rotateX(180deg)");
-        }
-    })
-});
