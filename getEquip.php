27492a50725b4ab76dfedd85fb6d59603a65225f
freemann350 <alex.gaspar@live.com.pt>
2017-06-27 14:50:42 +0100

    Add files via upload

---
+++ getEquip.php
@@ -0,0 +1,18 @@
+<?php
+  require_once("../Shared/conn.php");
+
+  $id = $_GET["id"];
+
+  $stmt = $con->prepare("SELECT * FROM equipamentos WHERE IdSala = ? ORDER BY Id ASC");
+  $stmt->bind_param("i", $id);
+
+  $stmt->execute();
+  $result = $stmt->get_result();
+?>
+  <option selected value="">Escolha um equipamento...</option>
+
+<?php
+  while ($row = $result->fetch_assoc()) {
+?>
+  <option value="<?=$row["Id"]?>"><?=$row["Nome"]?></option>
+<?php } ?>
